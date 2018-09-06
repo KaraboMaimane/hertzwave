ef84eeb31d3562a2fb9f2b2993bf9b315e1542c7
unknown <you@example.com>
2018-09-06 15:15:46 +0200

    shoni

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
 /**
  * Generated class for the RegisterPage page.
@@ -20,6 +21,10 @@ export class RegisterPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  register(form: NgForm){
+    console.log(form.value);
   }
 
 }
